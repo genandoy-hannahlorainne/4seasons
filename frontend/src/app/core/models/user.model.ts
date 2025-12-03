d13d629298442f9dafbc1161a51349070b04b0a8
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2025-12-03 18:23:47 +0800

    update readme and fix user model

--- frontend/src/app/core/models/user.model.ts
+++ frontend/src/app/core/models/user.model.ts
@@ -1,11 +1,15 @@
 export interface User {
   user_id: number;
   role_id: number;
+  role_name: string;
   username: string;
   email?: string;
   phone?: string;
   full_name?: string;
   is_active: boolean;
+  student_info?: any;
+  adviser_info?: any;
+  staff_info?: any;
 }
 
 export interface Role {
