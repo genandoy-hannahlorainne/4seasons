abd93141abe37bc898bc56079ffde6b6c31eb440
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2026-01-13 01:43:50 +0800

    Import (#36)
    * add dependencies for pdf and excel library
    
    * feat: implement the change password and remove change photo

--- backend/api/change-password.php
+++ backend/api/change-password.php
@@ -1,8 +1,8 @@
 <?php
 // CORS headers
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, user_id, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
 
